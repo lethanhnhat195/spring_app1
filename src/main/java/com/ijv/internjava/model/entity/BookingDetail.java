7a3589c9e58507844f1fbcb648b120c0c1fe6295
nguyenvando2801 <donguyen2801@gmail.com>
2023-06-29 21:39:35 +0700

    [VanDo] Create Entity and Create file BaseEntity

---
+++ src/main/java/com/ijv/internjava/model/entity/BookingDetail.java
@@ -0,0 +1,33 @@
+package com.ijv.internjava.model.entity;
+
+import com.ijv.internjava.model.dto.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "booking_detail", indexes = {
+        @Index(name = "booking_id", columnList = "booking_id"),
+        @Index(name = "service_id", columnList = "service_id")
+})
+@Getter
+@Setter
+public class BookingDetail extends BaseEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+    @Column(name = "note", length = 500)
+    private String note;
+
+}
