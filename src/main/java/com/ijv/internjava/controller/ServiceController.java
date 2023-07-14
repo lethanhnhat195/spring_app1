a8428a5471070691019bc3347c069d518232086f
khanh.nd <ndkhanh.18it4@vku.udn.vn>
2023-07-14 16:20:47 +0700

    [DuyKhanh] Create service management module

---
+++ src/main/java/com/ijv/internjava/controller/ServiceController.java
@@ -0,0 +1,55 @@
+package com.ijv.internjava.controller;
+
+import com.ijv.internjava.model.dto.ServiceDto;
+import com.ijv.internjava.service.ServiceService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor(onConstructor= @__(@Autowired))
+@RequestMapping("/services")
+public class ServiceController {
+    private ServiceService serviceService;
+
+    @GetMapping
+    public List<ServiceDto> findAll() {
+        return serviceService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServiceDto> detail(
+            @PathVariable(name = "id") Long id
+    ) {
+        ServiceDto serviceDtoResponse = serviceService.detail(id);
+        return ResponseEntity.ok().body(serviceDtoResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<ServiceDto> create(
+            @RequestBody ServiceDto serviceDtoRequest
+    ) {
+        ServiceDto serviceDtoResponse = serviceService.create(serviceDtoRequest);
+        return new ResponseEntity<>(serviceDtoResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ServiceDto> edit(
+            @PathVariable Long id,
+            @RequestBody ServiceDto serviceDtoRequest
+    ) {
+        ServiceDto serviceDtoResponse = serviceService.edit(serviceDtoRequest, id);
+        return ResponseEntity.ok().body(serviceDtoResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(
+            @PathVariable(name = "id") Long id
+    ) {
+        serviceService.delete(id);
+    }
+}
---
+++ src/main/java/com/ijv/internjava/service/impl/ServiceServiceImpl.java
@@ -0,0 +1,83 @@
+package com.ijv.internjava.service.impl;
+
+import com.ijv.internjava.mapper.ServiceImageMapper;
+import com.ijv.internjava.mapper.ServiceMapper;
+import com.ijv.internjava.model.dto.ServiceDto;
+import com.ijv.internjava.model.dto.ServiceImageDto;
+import com.ijv.internjava.model.entity.Service;
+import com.ijv.internjava.model.entity.ServicesImage;
+import com.ijv.internjava.repository.ServiceImageRepository;
+import com.ijv.internjava.repository.ServiceRepository;
+import com.ijv.internjava.service.ServiceService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@org.springframework.stereotype.Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class ServiceServiceImpl implements ServiceService {
+    private ServiceRepository serviceRepository;
+    private ServiceImageRepository serviceImageRepository;
+
+    @Override
+    public List<ServiceDto> findAll() {
+        return serviceRepository.findAll().stream()
+                .map(service -> ServiceMapper.getInstance().toDto(service))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ServiceDto detail(Long id) {
+        Service service = serviceRepository.findById(id).get();
+        return ServiceMapper.getInstance().toDto(service);
+    }
+
+    @Override
+    public ServiceDto create(ServiceDto serviceDto) {
+        Service newService = new Service();
+        newService.setName(serviceDto.getName());
+        newService.setServiceTime(serviceDto.getServiceTime());
+        newService.setPrice(serviceDto.getPrice());
+        newService.setDescription(serviceDto.getDescription());
+        Set<ServicesImage> serviceImages = new HashSet<>();
+        serviceRepository.save(newService);
+        for (ServiceImageDto serviceImageDto : serviceDto.getServiceImageDtos()) {
+            ServicesImage newServicesImage = new ServicesImage();
+            newServicesImage.setId(newServicesImage.getId());
+            newServicesImage.setService(newService);
+            newServicesImage.setNameFile(serviceImageDto.getFileName());
+            serviceImages.add(newServicesImage);
+            serviceImageRepository.save(newServicesImage);
+        }
+        newService.setServicesImages(serviceImages);
+        return ServiceMapper.getInstance().toDto(serviceRepository.save(newService));
+    }
+
+    @Override
+    public ServiceDto edit(ServiceDto serviceDto, Long id) {
+        Service service = serviceRepository.findById(id).get();
+        service.setName(serviceDto.getName());
+        service.setPrice(serviceDto.getPrice());
+        service.setServiceTime(serviceDto.getServiceTime());
+        service.setDescription(serviceDto.getDescription());
+        for (ServiceImageDto serviceImageDto : serviceDto.getServiceImageDtos()) {
+            ServicesImage servicesImage = ServiceImageMapper.getInstance().toEntity(serviceImageDto);
+            servicesImage.setService(service);
+            serviceImageRepository.save(servicesImage);
+        }
+        return ServiceMapper.getInstance().toDto(serviceRepository.save(service));
+    }
+
+    @Override
+    public void delete(long id) {
+        Service service = serviceRepository.findById(id).get();
+        for (ServicesImage serviceImage : service.getServicesImages()) {
+            serviceImageRepository.deleteById(serviceImage.getId());
+        }
+        serviceRepository.delete(service);
+    }
+}
